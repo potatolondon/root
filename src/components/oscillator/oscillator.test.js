9dd6c75e13be6708e4754d60d6267ab74af9bd5c
Liam Newmarch <liam@potatolondon.com>
2022-05-25 09:53:05 +0100

    Update oscillator tests to use start() and stop() methods

--- src/components/oscillator/oscillator.test.js
+++ src/components/oscillator/oscillator.test.js
@@ -9,12 +9,20 @@ describe('Oscillator', () => {
     expect(el.oscillator).to.equal(undefined);
   });
 
-  it('plays when note is on', async () => {
+  it('plays when start method is called', async () => {
     const el = await fixture(html` <wapp-osc></wapp-osc> `);
-    el.isNoteOn = true;
-    el.frequency = 440;
+    el.start(69); // A4 - 440 Hz
+    await expect(el.activeNotes.size).to.equal(1);
     await nextFrame();
-    await expect(el.oscillator.type).to.equal('sine');
-    await expect(el.oscillator.frequency.value).to.equal(440);
+    const oscillator = el.activeNotes.get(69);
+    await expect(oscillator.type).to.equal('sine');
+    await expect(oscillator.frequency.value).to.equal(440);
+  });
+
+  it('stops playing when stop method is called', async () => {
+    const el = await fixture(html` <wapp-osc></wapp-osc> `);
+    el.stop(69); // A4 - 440 Hz
+    await expect(el.activeNotes.size).to.equal(0);
+    await nextFrame();
   });
 });
