2a0ea0ad66c9d7b18e7188a9571230a441945b15
Purvi Trivedi <purvi.trivedi@potatolondon.com>
2022-08-03 15:44:04 +0100

    Add my awful filter curve

--- src/components/filter/filter.lit.js
+++ src/components/filter/filter.lit.js
@@ -17,10 +17,17 @@ export class Filter extends WappElement {
 
   initialFrequency = 1000;
 
+  exponentValue = 7;
+
+  dividingValue = 80.015;
+
   connectedCallback() {
     super.connectedCallback();
     this.filterNode.type = 'bandpass';
-    this.filterNode.frequency.setValueAtTime(1000, audioCtx.currentTime);
+    this.filterNode.frequency.setValueAtTime(
+      this.initialFrequency,
+      audioCtx.currentTime
+    );
     this.filterNode.gain.setValueAtTime(25, audioCtx.currentTime);
   }
 
@@ -31,52 +38,68 @@ export class Filter extends WappElement {
       this.filterNode.type = value;
     }
 
+    if (id.includes('gain')) {
+      this.filterNode.gain.setValueAtTime(value, audioCtx.currentTime);
+    }
+
     if (id.includes('frequency')) {
       const rangeInput = document.querySelector('#filter-frequency-range');
       const numberInput = document.querySelector('#filter-frequency-number');
 
       if (type === 'number') {
-        rangeInput.value = Math.log(value * 80) * 7;
+        rangeInput.value = this._numberToRange(value);
       } else {
-        numberInput.value = Math.exp(parseInt(value, 10) / 10.4);
+        numberInput.value = Math.floor(
+          Math.exp(parseInt(value, 10) / this.exponentValue) /
+            this.dividingValue
+        );
         this.filterNode.frequency.setValueAtTime(value, audioCtx.currentTime);
       }
     }
   }
 
+  _numberToRange(value) {
+    return Math.log(value * this.dividingValue) * this.exponentValue;
+  }
+
   render() {
     return html`
-      <label for="filter-type">Filter Type</label>
-      <select id="filter-type" @input=${this.__oninput}>
-        ${map(
-          Object.entries(Filter.filterTypes),
-          ([value, label]) => html`
-            <option ?selected=${value === this.filterNode.type} value=${value}>
-              ${label}
-            </option>
-          `
-        )}
-      </select>
       <div>
-        <label for="filter-frequency">Filter Frequency: </label>
-        <input
-          @input=${this.__oninput}
-          type="range"
-          value="1"
-          id="filter-frequency-range"
-          name="filter-frequency"
-          min="0"
-          max="127"
-        />
-        <input
-          @input=${this.__oninput}
-          type="number"
-          value="1000"
-          id="filter-frequency-number"
-          name="filter-frequency"
-          min="20"
-          max="20000"
-        />
+        <label for="filter-type">Filter Type</label>
+        <select id="filter-type" @input=${this.__oninput}>
+          ${map(
+            Object.entries(Filter.filterTypes),
+            ([value, label]) => html`
+              <option
+                ?selected=${value === this.filterNode.type}
+                value=${value}
+              >
+                ${label}
+              </option>
+            `
+          )}
+        </select>
+        <div>
+          <label for="filter-frequency">Filter Frequency: </label>
+          <input
+            @input=${this.__oninput}
+            type="range"
+            .value=${this._numberToRange(this.initialFrequency)}
+            id="filter-frequency-range"
+            name="filter-frequency"
+            min="0"
+            max="100"
+          />
+          <input
+            @input=${this.__oninput}
+            type="number"
+            .value=${this.initialFrequency}
+            id="filter-frequency-number"
+            name="filter-frequency"
+            min="20"
+            max="20000"
+          />
+        </div>
       </div>
     `;
   }
