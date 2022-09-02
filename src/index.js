e4e3f7fc10efa4aee6d16d3f972a0f9f413f161f
Purvi Trivedi <purvi.trivedi@potatolondon.com>
2022-09-02 16:21:44 +0100

    Bring back user input

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import './components/synth/synth.lit.js';
 import './components/user-input/user-input.lit.js';
 import './components/midi-input/midi-input.lit.js';
 
-render(html` <wapp-synth></wapp-synth> `, document.querySelector('#synth'));
+render(
+  html`
+    <wapp-synth></wapp-synth>
+    <user-input></user-input>
+  `,
+  document.querySelector('#synth')
+);
