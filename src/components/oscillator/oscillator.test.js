f968d0e46925b1f3c095d4974dc099ab0e604011
Liam Newmarch <liam@potatolondon.com>
2022-09-22 17:42:17 +0100

    Add tests for detune/pitch-bend

--- src/components/oscillator/oscillator.test.js
+++ src/components/oscillator/oscillator.test.js
@@ -16,19 +16,62 @@ describe('Oscillator', () => {
   it('plays when start method is called', async () => {
     /** @type {Oscillator} */
     const el = await fixture(html` <wapp-osc></wapp-osc> `);
+
     el.start(69); // A4 - 440 Hz
-    await expect(el.activeNotes.size).to.equal(1);
+    expect(el.activeNotes.size).to.equal(1);
+
     await nextFrame();
+
     const oscillator = el.activeNotes.get(69);
-    await expect(oscillator.type).to.equal('sine');
-    await expect(oscillator.frequency.value).to.equal(440);
+    expect(oscillator.type).to.equal('sine');
+    expect(oscillator.frequency.value).to.equal(440);
   });
 
   it('stops playing when stop method is called', async () => {
     /** @type {Oscillator} */
     const el = await fixture(html` <wapp-osc></wapp-osc> `);
     el.stop(69); // A4 - 440 Hz
-    await expect(el.activeNotes.size).to.equal(0);
-    await nextFrame();
+    expect(el.activeNotes.size).to.equal(0);
+  });
+
+  it('responds to detune', async () => {
+    /** @type {Oscillator} */
+    const el = await fixture(html` <wapp-osc></wapp-osc> `);
+
+    /** @type {HTMLInputElement?} */
+    const detuneInput = el.querySelector('#detune');
+    if (!detuneInput) throw new Error('<wapp-osc> does not contain #detune');
+
+    detuneInput.value = '-1';
+    detuneInput.dispatchEvent(new InputEvent('input'));
+    expect(el.detune).to.equal(-200);
+    expect(el.stickyPitchBend).to.be.false;
+
+    detuneInput.dispatchEvent(new MouseEvent('mouseup'));
+    expect(el.detune).to.equal(0);
+  });
+
+  it('detune stickiness', async () => {
+    /** @type {Oscillator} */
+    const el = await fixture(html` <wapp-osc></wapp-osc> `);
+
+    /** @type {HTMLInputElement?} */
+    const stickyInput = el.querySelector('#sticky');
+    if (!stickyInput) throw new Error('<wapp-osc> does not contain #sticky');
+
+    stickyInput.checked = true;
+    stickyInput.dispatchEvent(new InputEvent('input'));
+
+    /** @type {HTMLInputElement?} */
+    const detuneInput = el.querySelector('#detune');
+    if (!detuneInput) throw new Error('<wapp-osc> does not contain #detune');
+
+    detuneInput.value = '-1';
+    detuneInput.dispatchEvent(new InputEvent('input'));
+    expect(el.detune).to.equal(-200);
+    expect(el.stickyPitchBend).to.be.true;
+
+    detuneInput?.dispatchEvent(new MouseEvent('mouseup'));
+    expect(el.detune).to.equal(-200);
   });
 });
