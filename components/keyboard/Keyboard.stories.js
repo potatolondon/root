e5d720bc4a1f98ec1ef7546412907c04ce084014
Liam Newmarch <liam@potatolondon.com>
2022-10-04 19:03:59 +0100

    Fix broken storybook CSS reference

--- components/keyboard/Keyboard.stories.js
+++ components/keyboard/Keyboard.stories.js
@@ -1,7 +1,7 @@
 import { html } from 'lit-html';
 
 import './keyboard.lit.ts';
-import '../../index.css';
+import '../../demo/index.css'; // TODO move keyboard styles to ./keyboard.css
 
 import KeyboardDocumentation from './KeyboardDocumentation.mdx';
 
