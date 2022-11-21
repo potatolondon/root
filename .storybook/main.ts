9ff2d9f7d0f359e8d748207015afc03580cec280
Purvi Trivedi <purvi.trivedi@potatolondon.com>
2022-11-21 22:08:01 +0000

    Fix storybook paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ module.exports = {
     '../components/**/*.stories.mdx',
     '../components/**/*.stories.@(js|jsx|ts|tsx)',
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   framework: '@storybook/web-components',
