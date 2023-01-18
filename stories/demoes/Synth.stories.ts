f67e1c68e5c3f1f2d46a248dca051566907cbf86
Purvi Trivedi <purvi.trivedi@potatolondon.com>
2023-01-18 10:44:37 +0000

    Add synths to storybook

--- stories/demoes/Synth.stories.ts
+++ stories/demoes/Synth.stories.ts
@@ -11,7 +11,7 @@ export default {
   parameters: {},
 };
 
-const Template = () => html` <root-midi></root-midi>
+const SingleOscillator = () => html` <root-midi></root-midi>
   <root-keyboard></root-keyboard>
   <root-connect>
     <root-osc id="osc-1" sendTo="filter-1"></root-osc>
@@ -22,4 +22,32 @@ const Template = () => html` <root-midi></root-midi>
     ></root-filter>
   </root-connect>`;
 
-export const Basic = Template.bind({});
+export const Basic = SingleOscillator.bind({});
+
+const OscillatorModule = () => html` <root-midi></root-midi>
+  <root-keyboard></root-keyboard>
+  <root-connect>
+    <root-osc-module id="osc-2" sendTo="filter-2"></root-osc-module>
+    <root-filter
+      id="filter-2"
+      sendTo="output"
+      recieveFrom="osc-2"
+    ></root-filter>
+  </root-connect>`;
+
+export const SynthOscillatorModule = OscillatorModule.bind({});
+
+
+const TwoOscTypes = () => html` <root-midi></root-midi>
+  <root-keyboard></root-keyboard>
+  <root-connect>
+    <root-osc id="osc-3" sendTo="filter-3"></root-osc>
+    <root-osc-module id="osc-4" sendTo="filter-3"></root-osc-module>
+    <root-filter
+      id="filter-3"
+      sendTo="output"
+      recieveFrom="osc-3 osc-4"
+    ></root-filter>
+  </root-connect>`;
+
+export const SynthTwoOscillatorTypes = TwoOscTypes.bind({});
