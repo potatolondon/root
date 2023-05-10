ce2587bc406bee47d9c173e42bc05be3b4abd6b2
Liam Newmarch <liam@potatolondon.com>
2023-05-10 21:20:05 +0100

    Add storybook-css-modules-preset to dev dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,10 @@ module.exports = {
     '../stories/**/*.stories.@(js|jsx|ts|tsx)',
     '../stories/*.stories.@(js|jsx|ts|tsx|mdx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-css-modules-preset',
+  ],
   framework: '@storybook/web-components',
 };
