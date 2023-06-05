fc2c66a257e292a2001bdbd1e038775f64ec2697
Liam Newmarch <liam@potatolondon.com>
2023-06-05 14:16:17 +0100

    Move demo files into the demo directory

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,10 @@
-import '../demo/index.css';
+import '../themes/default/base.css';
+import '../components/display/display.css';
 import '../components/fader/fader.css';
 import '../components/filter/filter.css';
+import '../components/keyboard/keyboard.css';
+import '../components/oscillator/oscillator-module.css';
+import '../components/oscillator/oscillator.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
